8b2592b8e049ad8ad88e3703ad2b0e8f8f96fad0
dixittanisha02 <dixittanisha02@gmail.com>
2024-04-15 23:21:55 +0530

    feat: creates component widget BottomNavBarItems

---
+++ lib/presentation/bottom_navbar/components/bottom_navbar_items.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+List<BottomNavigationBarItem> BottomNavBarItems =
+    const <BottomNavigationBarItem>[
+  BottomNavigationBarItem(
+    icon: Icon(Icons.plus_one_outlined),
+    label: "Counter",
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(Icons.change_circle_rounded),
+    label: "Converter",
+  ),
+];
