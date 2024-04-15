8d9ff995b24a6b666449aa78fec526af06c2783b
dixittanisha02 <dixittanisha02@gmail.com>
2024-04-15 23:30:38 +0530

    refac: updates CounterState

--- lib/presentation/counter/bloc/counter_state.dart
+++ lib/presentation/counter/bloc/counter_state.dart
@@ -1,22 +1,22 @@
 part of 'counter_bloc.dart';
 
-abstract class CounterState {}
-
-class CounterInitial extends CounterState {
-  final int count;
-
-  CounterInitial(this.count);
+abstract class CounterState {
+  int count;
+  CounterState(this.count);
 }
+
+// class CounterInitial extends CounterState {
+//   final int count;
+
+//   CounterInitial(this.count);
+// }
 
 class CounterUpdated extends CounterState {
-  final int count;
-
-  CounterUpdated(this.count);
+  CounterUpdated(super.count);
 }
 
-class CounterError extends CounterState {
-  final String message;
+// class CounterError extends CounterState {
+//   final String message;
 
-  CounterError(this.message);
-}
-
+//   CounterError(this.message);
+// }
