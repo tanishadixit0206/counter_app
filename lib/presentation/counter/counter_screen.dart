bfdb81971544b8c6c3e2f8417b1529a4c58f86a0
dixittanisha02 <dixittanisha02@gmail.com>
2024-04-15 23:29:36 +0530

    feat: adds BlocConsumer , adds ui

--- lib/presentation/counter/counter_screen.dart
+++ lib/presentation/counter/counter_screen.dart
@@ -1,3 +1,4 @@
+import 'package:counter_app/presentation/bottom_navbar/bottom_navbar_screen.dart';
 import 'package:counter_app/presentation/counter/bloc/counter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,27 +9,72 @@ class CounterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-        create: (context) => CounterBloc(),
-        child: Scaffold(
-            body: Column(
-          children: [
-            AppBar(
-              title: Text(
-                "Counter Screen",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.5,
+      create: (context) => CounterBloc(),
+      child: BlocConsumer<CounterBloc, CounterState>(
+          listener: (context, state) {},
+          builder: (context, state) {
+            return Scaffold(
+                body: Column(
+              children: [
+                AppBar(
+                  title: Text(
+                    "Counter Screen",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.5,
+                    ),
+                  ),
+                  backgroundColor: Colors.purple.shade100,
+                  centerTitle: true,
                 ),
-              ),
-              backgroundColor: Colors.purple.shade100,
-              centerTitle: true,
-            ),
-            Expanded(
-              child: Container(
-                  decoration: BoxDecoration(color: Colors.purple.shade50),),
-            ),
-            
-          ],
-        )));
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.purple.shade50),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton.icon(
+                              onPressed: () {
+                                context.read<CounterBloc>().add(Increment());
+                              },
+                              icon: Icon(Icons.add),
+                              label: Text(""),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(16.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.purple.shade100),
+                                child: Center(
+                                  child: Text("Count: ${state.count}"),
+                                ),
+                              ),
+                            ),
+                            ElevatedButton.icon(
+                              onPressed: () {
+                                context.read<CounterBloc>().add(Decrement());
+                              },
+                              icon: Icon(
+                                Icons.remove,
+                              ),
+                              label: Text(""),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  child: BottomNavbarScreen(),
+                  height: 60,
+                ),
+              ],
+            ));
+          }),
+    );
   }
 }
