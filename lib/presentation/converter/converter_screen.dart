22ec4503c729490cc0d22b441535fb48e0873775
dixittanisha02 <dixittanisha02@gmail.com>
2024-04-15 23:22:26 +0530

    feat: creates converter_screen

--- lib/presentation/converter/converter_screen.dart
+++ lib/presentation/converter/converter_screen.dart
@@ -1,14 +1,15 @@
+import 'package:counter_app/presentation/bottom_navbar/bottom_navbar_screen.dart';
 import 'package:counter_app/presentation/converter/bloc/converter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CounterScreen extends StatelessWidget {
-  const CounterScreen({Key? key}) : super(key: key);
+class ConverterScreen extends StatelessWidget {
+  const ConverterScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-        create: (context) => ,
+        create: (context) => ConverterBloc(),
         child: Scaffold(
             body: Column(
           children: [
@@ -25,9 +26,46 @@ class CounterScreen extends StatelessWidget {
             ),
             Expanded(
               child: Container(
-                  decoration: BoxDecoration(color: Colors.purple.shade50),),
+                  decoration: BoxDecoration(color: Colors.purple.shade50),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Decimal:"),
+                            Container(
+                              decoration:
+                                  BoxDecoration(color: Colors.purple.shade100),
+                              child: Text("A"),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Binary:"),
+                            Container(
+                              decoration:
+                                  BoxDecoration(color: Colors.purple.shade100),
+                              child: Text("A"),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(onPressed: () {}, child: Text("Convert"))
+                    ],
+                  )),
             ),
-            
+            SizedBox(
+              child: BottomNavbarScreen(),
+              height: 60,
+            ),
           ],
         )));
   }
