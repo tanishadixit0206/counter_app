f41bcc050fcda4466f5d9195b12828268cdd614e
dixittanisha02 <dixittanisha02@gmail.com>
2024-04-15 23:20:49 +0530

    feat: creates bottom_navbar_bloc

---
+++ lib/presentation/bottom_navbar/bloc/bottom_navbar_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+part 'bottom_navbar_event.dart';
+part 'bottom_navbar_state.dart';
+
+class BottomNavbarBloc extends Bloc<BottomNavbarEvent, BottomNavbarState> {
+  BottomNavbarBloc() : super(const BottomNavbarCurrentState(index: 0)) {
+    on<BottomNavbarEvent>((event, emit) {
+      if (event is TabChange) {
+        _onTabChange(event.index);
+      }
+    });
+  }
+
+  void _onTabChange(int index) {
+    emit(BottomNavbarCurrentState(index: index));
+    // navigateTo(index);
+  }
+
+  void navigateTo(BuildContext context, int index) {
+    if (state is BottomNavbarCurrentState) {
+      switch (index) {
+        case 0:
+          Navigator.of(context).pushNamed('/counter');
+          break;
+        case 1:
+          Navigator.of(context).pushNamed('/converter');
+          break;
+      }
+    }
+  }
+}
