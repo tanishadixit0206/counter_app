8b243e2addbdd61a103b4b0437b4823db6674340
dixittanisha02 <dixittanisha02@gmail.com>
2024-04-17 10:53:25 +0530

    feat: adds textfields and onpressed callbacks

--- lib/presentation/converter/converter_screen.dart
+++ lib/presentation/converter/converter_screen.dart
@@ -4,69 +4,144 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ConverterScreen extends StatelessWidget {
-  const ConverterScreen({Key? key}) : super(key: key);
+  ConverterScreen({Key? key}) : super(key: key);
+  TextEditingController decimalcontroller = TextEditingController();
+  TextEditingController binarycontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-        create: (context) => ConverterBloc(),
-        child: Scaffold(
+      create: (context) => ConverterBloc(),
+      child: BlocConsumer<ConverterBloc, ConverterState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Scaffold(
             body: Column(
-          children: [
-            AppBar(
-              title: Text(
-                "Converter Screen",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.5,
+              children: [
+                AppBar(
+                  title: Text(
+                    "Converter Screen",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.5,
+                    ),
+                  ),
+                  backgroundColor: Colors.purple.shade100,
+                  centerTitle: true,
                 ),
-              ),
-              backgroundColor: Colors.purple.shade100,
-              centerTitle: true,
-            ),
-            Expanded(
-              child: Container(
-                  decoration: BoxDecoration(color: Colors.purple.shade50),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text("Decimal:"),
-                            Container(
-                              decoration:
-                                  BoxDecoration(color: Colors.purple.shade100),
-                              child: Text("A"),
-                            ),
-                          ],
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.purple.shade50),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  controller: decimalcontroller,
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                  ),
+                                  decoration: InputDecoration(
+                                    labelText: "Decimal",
+                                    labelStyle: TextStyle(
+                                      fontSize: 16.0,
+                                      color: Colors.grey,
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.grey, width: 1.0),
+                                      borderRadius: BorderRadius.circular(8.0),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .primary,
+                                          width: 1.5),
+                                      borderRadius: BorderRadius.circular(8.0),
+                                    ),
+                                    contentPadding: EdgeInsets.symmetric(
+                                        vertical: 14.0, horizontal: 16.0),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text("Binary:"),
-                            Container(
-                              decoration:
-                                  BoxDecoration(color: Colors.purple.shade100),
-                              child: Text("A"),
-                            ),
-                          ],
+                        Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  controller: binarycontroller,
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                  ),
+                                  decoration: InputDecoration(
+                                    labelText: "Binary",
+                                    labelStyle: TextStyle(
+                                      fontSize: 16.0,
+                                      color: Colors.grey,
+                                    ),
+                                    enabledBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Colors.grey, width: 1.0),
+                                      borderRadius: BorderRadius.circular(8.0),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .primary,
+                                          width: 1.5),
+                                      borderRadius: BorderRadius.circular(8.0),
+                                    ),
+                                    contentPadding: EdgeInsets.symmetric(
+                                        vertical: 14.0, horizontal: 16.0),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      ElevatedButton(onPressed: () {}, child: Text("Convert"))
-                    ],
-                  )),
+                        ElevatedButton(
+                          onPressed: () {
+                            print(decimalcontroller.text);
+                            print(binarycontroller.text);
+                            state.decimal = decimalcontroller.text;
+                            state.binary = binarycontroller.text;
+                            if (state.decimal != "") {
+                              context
+                                  .read<ConverterBloc>()
+                                  .add(DecimalToBinaryEvent());
+                            } else if (state.binary != "") {
+                              context
+                                  .read<ConverterBloc>()
+                                  .add(BinaryToDecimalEvent());
+                            }
+                          },
+                          child: Text("Convert"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  child: BottomNavbarScreen(),
+                  height: 60,
+                ),
+              ],
             ),
-            SizedBox(
-              child: BottomNavbarScreen(),
-              height: 60,
-            ),
-          ],
-        )));
+          );
+        },
+      ),
+    );
   }
 }
