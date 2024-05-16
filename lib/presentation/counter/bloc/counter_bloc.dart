18d5ec0c3369612197a376310be3184a8e62ac07
dixittanisha02 <dixittanisha02@gmail.com>
2024-05-16 22:47:00 +0530

    rfac modifies onIncrement and onDecrement functions to update count variable rather than updating the state variable

--- lib/presentation/counter/bloc/counter_bloc.dart
+++ lib/presentation/counter/bloc/counter_bloc.dart
@@ -7,18 +7,18 @@ part 'counter_event.dart';
 part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(CounterUpdated(0)) {
+  CounterBloc() : super(CounterState(0)) {
     on<Increment>(_onIncrement);
     on<Decrement>(_onDecrement);
   }
 
   void _onIncrement(event, emit) {
-    state.count++;
-    emit(CounterUpdated(state.count));
+    int count = state.count;
+    emit(CounterState(count++));
   }
 
   void _onDecrement(event, emit) {
-    state.count--;
-    emit(CounterUpdated(state.count));
+    int count = state.count;
+    emit(CounterState(count++));
   }
 }
