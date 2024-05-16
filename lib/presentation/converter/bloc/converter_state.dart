8804c1e8d727368190096ad1ea9c031b97f57832
dixittanisha02 <dixittanisha02@gmail.com>
2024-05-16 22:45:09 +0530

    feat removes the parameters from the abstract class and adds text1 and text2 parameters to the BinaryToDecimalState and DecimalToBinaryState to display the mode of conversion on the ConverterScreen

--- lib/presentation/converter/bloc/converter_state.dart
+++ lib/presentation/converter/bloc/converter_state.dart
@@ -1,15 +1,17 @@
 part of 'converter_bloc.dart';
 
-abstract class ConverterState {
-  String binary;
-  String decimal;
-  ConverterState(this.binary, this.decimal);
-}
+abstract class ConverterState {}
 
 class BinaryToDecimalState extends ConverterState {
-  BinaryToDecimalState(super.binary, super.decimal);
+  static const String text1="Binary";
+  static const String text2="Decimal";
+  final String decimal;
+  BinaryToDecimalState(this.decimal);
 }
 
 class DecimalToBinaryState extends ConverterState {
-  DecimalToBinaryState(super.binary, super.decimal);
+  static const String text1="Decimal";
+  static const String text2="Binary";
+  final String binary;
+  DecimalToBinaryState(this.binary);
 }
