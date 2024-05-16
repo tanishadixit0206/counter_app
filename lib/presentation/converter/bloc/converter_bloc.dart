a753d0e698c2a8297fc7db539eb1454aee3f5115
dixittanisha02 <dixittanisha02@gmail.com>
2024-05-16 22:45:55 +0530

    feat adds onConvert, onSwap and onReset functions

--- lib/presentation/converter/bloc/converter_bloc.dart
+++ lib/presentation/converter/bloc/converter_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:counter_app/data/services/remote/api_service.dart';
 import 'package:counter_app/domain/repositories/conversion_repository.dart';
 import 'package:flutter/material.dart';
@@ -9,25 +11,43 @@ part 'converter_event.dart';
 part 'converter_state.dart';
 
 class ConverterBloc extends Bloc<ConverterEvent, ConverterState> {
-  ConverterBloc() : super(DecimalToBinaryState("", "")) {
-    on<BinaryToDecimalEvent>(_onBinaryToDecimal);
-    on<DecimalToBinaryEvent>(_onDecimalToBinary);
+  final ConversionRepository conversionRepository;
+  ConverterBloc(this.conversionRepository) : super(DecimalToBinaryState("")) {
+    on<ConvertEvent>(_onConvert);
+    on<SwapConversionEvent>(_onSwap);
+    on<ResetConversionEvent>(_onReset);
   }
-  Future<void> _onBinaryToDecimal(event, emit) async {
-    emit(BinaryToDecimalState(state.binary, state.decimal));
-    print(state.decimal);
-    print(state.binary);
-    state.decimal = await binaryToDecimal(state.binary);
-    print(state.decimal);
-    print(state.binary);
+  Future<void> _onConvert(event, emit) async {
+    if (state is DecimalToBinaryState) {
+      String output = await conversionRepository.decimalToBinary(event.input);
+      emit(DecimalToBinaryState(output));
+    } else if (state is BinaryToDecimalState) {
+      String output = await conversionRepository.binaryToDecimal(event.input);
+      emit(BinaryToDecimalState(output));
+    }
   }
 
-  Future<void> _onDecimalToBinary(event, emit) async {
-    emit(DecimalToBinaryState(state.binary, state.decimal));
-    print(state.decimal);
-    print(state.binary);
-    state.binary = await decimalToBinary(state.decimal);
-    print(state.decimal);
-    print(state.binary);
+  Future<void> _onSwap(event, emit) async {
+    if (state is BinaryToDecimalState) {
+      emit(
+        DecimalToBinaryState(""),
+      );
+    } else if (state is DecimalToBinaryState) {
+      emit(
+        BinaryToDecimalState(""),
+      );
+    }
+  }
+
+  Future<void> _onReset(event, emit) async {
+    if (state is BinaryToDecimalState) {
+      emit(
+        BinaryToDecimalState(""),
+      );
+    } else if (state is DecimalToBinaryState) {
+      emit(
+        DecimalToBinaryState(""),
+      );
+    }
   }
 }
